58c9ccc0c7a0236023ac380ad4f5be7b4e8c3f99
Sagi <sagi@noosa.io>
2021-01-10 16:44:57 +0200

    created cart item compoenent

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,14 @@ import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+
 import { auth } from "../../firebase/firebase.utils";
 
 import { connect } from "react-redux";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
+const Header = ({currentUser, hidden}) => {
 	return (
 		<div className="header">
 			<Link to="/" className="logo-container">
@@ -29,13 +32,16 @@ const Header = ({ currentUser }) => {
 						SIGN IN
 					</Link>
 				)}
+				<CartIcon />
 			</div>
+			{hidden ? null : <CartDropdown />}
 		</div>
 	);
 };
 
-const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
 });
 
 export default connect(mapStateToProps)(Header);
