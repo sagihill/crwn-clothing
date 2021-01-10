501afbffb94105accc302c50d124a2f77357743e
Sagi <sagi@noosa.io>
2021-01-10 12:43:56 +0200

    added redux

--- src/App.js
+++ src/App.js
@@ -7,33 +7,29 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import { auth, createUserProfileDoc } from "./firebase/firebase.utils";
 
+import { connect } from "react-redux";
+
+import { setCurrentUser } from "./redux/user/user.actions";
+
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			currentUser: null,
-		};
-	}
-
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
+		const { setCurrentUser } = this.props;
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDoc(userAuth);
 				userRef.onSnapshot((snapShot) => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
-							...snapShot.data(),
-						},
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data(),
 					});
 				});
 			} else {
-				this.setState({ currentUser: null });
+				setCurrentUser(null);
 			}
 		});
 	}
@@ -45,7 +41,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
@@ -56,4 +52,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
