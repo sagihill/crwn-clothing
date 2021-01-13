41db63fca30b3d78d8fef9d74a321eefe778e6f1
Sagi <sagi@noosa.io>
2021-01-13 18:23:01 +0200

    minor fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ import { persistStore } from "redux-persist";
 
 const middlewares = [];
 
-if ((process.env.NODE_ENV = "development")) {
+if ((process.env.NODE_ENV === "development")) {
 	middlewares.push(logger);
 }
 
